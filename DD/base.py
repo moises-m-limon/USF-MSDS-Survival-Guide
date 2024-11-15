32d0ce177a7dc2192f41051f8a09ee577a7ed1bb
moises-m-limon <moises.m.limon@gmail.com>
2024-11-15 14:00:58 -0800

    added DD

---
+++ DD/base.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import os
+from base import list_notebooks, display_notebook  # Import shared logic
+
+def main():
+    st.header("Distributed Computing")
+    current_folder = os.path.dirname(__file__)
+    
+    # List all notebooks in the current folder
+    notebooks = list_notebooks(current_folder)
+    
+    # Sidebar to select a notebook
+    selected_notebook = st.sidebar.selectbox("Select a Notebook", notebooks)
+    
+    if selected_notebook:
+        display_notebook(current_folder, selected_notebook)
+
+if __name__ == "__main__":
+    main()
